f8edc59bd1ccd173ca91d982d4a2ef8908416509
Joshua Kappers <josh@jkappers.me>
2014-08-01 12:15:05 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TownHall::Application.config.secret_key_base = 'c972f5ff8871ab225070fa4ddfee072f82e70dc25ebb1140de6ac38d77af486a50ea219ad438885a47c27ffc0fbd1e11732901d8cf1547214ba4e2ce082b2d0e'
