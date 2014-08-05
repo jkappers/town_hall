3df85add3cf8da49f4fc6b963895bb236d0bd2d7
Joshua Kappers <josh@jkappers.me>
2014-08-05 17:18:01 -0500

    Just some plain old bootstrap stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ User.create(name: "Joshua Kappers", email: "jkappers@kalkomey.com", password: "1
 User.create(name: "Jay Sparks", email: "jsparks@kalkomey.com", password: "1.kalkomey.1", votes: 100)
 User.create(name: "Bradley Griffith", email: "bgriffith@kalkomey.com", password: "1.kalkomey.1", votes: 100)
 
-Card.create(votes: 10, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet tempus lorem euismod dictum. Etiam eu nullam.")
-Card.create(votes: 5, description: "Phasellus faucibus, risus sed rhoncus eleifend, ligula purus hendrerit mauris, sit amet vulputate sapien arcu at nullam.")
-Card.create(votes: 1, description: "Nunc tempus nunc sit amet mi venenatis venenatis. Nulla eu nisl tincidunt, elementum elit ut, lobortis neque massa nunc.")
+Card.create(description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet tempus lorem euismod dictum. Etiam eu nullam.")
+Card.create(description: "Phasellus faucibus, risus sed rhoncus eleifend, ligula purus hendrerit mauris, sit amet vulputate sapien arcu at nullam.")
+Card.create(description: "Nunc tempus nunc sit amet mi venenatis venenatis. Nulla eu nisl tincidunt, elementum elit ut, lobortis neque massa nunc.")
