aa87ab907fbad0fb10c099b1aaac7785d61cb6ef
Joshua Kappers <josh@jkappers.me>
2014-08-05 22:18:52 -0500

    No need for Angular Route since we're using UI Router

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,12 @@ requirejs.config({
   waitSeconds: 200,
   paths: {
     angular           : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular.min',
-    angularRoute      : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular-route.min',
     angularResource   : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular-resource.min',
     angularAnimate    : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular-animate.min',
     uiRouter          : '/vendor/js/angular-ui-router'
   },
   shim: {
     'angular'           : {'exports': 'angular'},
-    'angularRoute'      : ['angular'],
     'angularAnimate'    : ['angular'],
     'angularResource'   : ['angular'],
     'uiRouter'          : ['angular']
@@ -24,12 +22,11 @@ window.name = 'NG_DEFER_BOOTSTRAP!';
 
 define([
   'angular',
-  'angularRoute',
   'angularAnimate',
   'uiRouter',
   'app',
   'routes'
-], function(angular, angularRoute, angularAnimate, uiRouter, app, routes) {
+], function(angular, angularAnimate, uiRouter, app, routes) {
   'use strict';
   var $html = angular.element(document.getElementsByTagName('html')[0]);
 
